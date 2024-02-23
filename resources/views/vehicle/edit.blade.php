e8a16dddf9ba627cd1af2d0970ca8b4350f8723d
ilham_ath <ilham.athallah08@gmail.com>
2024-02-23 14:16:02 +0700

    new

--- resources/views/vehicle/edit.blade.php
+++ resources/views/vehicle/edit.blade.php
@@ -14,8 +14,12 @@
 
             <label for="">Name update</label>
             <input type="text" value="{{ $vehicle->name }}" class="form-control" name="name">
-            
-            <button type="submit" class="mt-3 btn btn-warning">update</button>
+
+            <button type="submit" class="mt-3 btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                    height="24" viewBox="0 0 24 24" fill="white">
+                    <path d="M11 15h2V9h3l-4-5-4 5h3z"></path>
+                    <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z"></path>
+                </svg></button>
         </form>
 
     </div>
