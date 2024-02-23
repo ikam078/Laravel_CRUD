376c5e3387911bf613ecbd4b8d1b84a4c4a6e8a8
ilham_ath <ilham.athallah08@gmail.com>
2024-02-23 14:15:10 +0700

    new

--- resources/views/vehicle/create.blade.php
+++ resources/views/vehicle/create.blade.php
@@ -13,18 +13,15 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label for="inputName">Vehicle Type</label>
-                        <input type="text" class="form-control" id="inputName" placeholder="Type..."
-                            name="name">
+                        <input type="text" class="form-control" id="inputName" placeholder="Type..." name="name">
                     </div>
                     <div class="form-group">
                         <label for="inputColor">Vehicle Color</label>
-                        <input type="text" class="form-control" id="inputColor" placeholder="Color..."
-                            name="color">
+                        <input type="text" class="form-control" id="inputColor" placeholder="Color..." name="color">
                     </div>
                     <div class="form-group">
                         <label for="inputColor">Amount of Tires</label>
-                        <input type="number" class="form-control" id="inputColor" placeholder="4"
-                            name="tire_wheel">
+                        <input type="number" class="form-control" id="inputColor" placeholder="4" name="tire_wheel">
                     </div>
                     <div class="form-group">
                         <label for="inputMachine">Machine Model</label>
@@ -33,14 +30,18 @@
                     </div>
                     <div class="form-group">
                         <label for="inputPrice">Determine Price</label>
-                        <input type="text" class="form-control" id="inputPrice" placeholder="$..."
-                            name="price">
+                        <input type="text" class="form-control" id="inputPrice" placeholder="$..." name="price">
                     </div>
                 </div>
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-primary float-left">Create</button>
+                    <button type="submit" class="btn btn-primary float-left"><svg xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" viewBox="0 0 24 24"
+                            fill="white">
+                            <path d="M11 15h2V9h3l-4-5-4 5h3z"></path>
+                            <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z"></path>
+                        </svg></button>
                 </div>
             </form>
         </div>
